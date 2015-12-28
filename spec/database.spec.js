94d51c22a828823a61cea3f4b6ccfc0a6795be85
Fabian Furger <furgerfabian@hotmail.com>
2015-12-28 12:21:03 +0100

    added some database tests

--- spec/database.spec.js
+++ spec/database.spec.js
@@ -1,2 +1,95 @@
 'use strict';
 
+var rewire = require('rewire'),
+  database = rewire('./../src/database'),
+  Subscription = database.Subscription;
+
+describe('subscription', function () {
+  var minimalSubscription,
+    extendedSubscription,
+    subscriptions;
+
+  beforeEach(function () {
+    var preSaveAction;
+
+    // create subscriptions
+    minimalSubscription = new Subscription({ name: 'test minimal' });
+    extendedSubscription = new Subscription({ name: 'test extended', searchParameters: 'a b c', lastSeason: 12, lastEpisode: 34 });
+
+    // extract, assign and execute preSaveAction
+    // this is "necessary" to avoid calling Subscription.save()
+    // which would include actual database interaction (and thus
+    // require an actual database instance for the tests to run)
+
+    // extract preSaveAction
+    preSaveAction = database.__get__('preSaveAction');
+    // assign preSaveAction to the subscriptions
+    minimalSubscription.preSaveAction = preSaveAction;
+    extendedSubscription.preSaveAction = preSaveAction;
+    // execute preSaveAction for the subscriptions
+    minimalSubscription.preSaveAction(function () {});
+    extendedSubscription.preSaveAction(function () {});
+
+    // store array with all pre-defined test subscriptions
+    subscriptions  = [ minimalSubscription, extendedSubscription ];
+  });
+
+  it('should assign the expected default values', function () {
+    expect(minimalSubscription.name).toBe('test minimal');
+    expect(minimalSubscription.searchParameters).toBeUndefined();
+    expect(minimalSubscription.lastSeason).toBe(1);
+    expect(minimalSubscription.lastEpisode).toBe(0);
+    expect(typeof minimalSubscription.creationTime).toBe('object');
+    expect(typeof minimalSubscription.lastModified).toBe('object');
+    expect(minimalSubscription.lastSeasonUpdateCheck).toBeUndefined();
+    expect(minimalSubscription.lastEpisodeUpdateCheck).toBeUndefined();
+
+    expect(extendedSubscription.name).toBe('test extended');
+    expect(extendedSubscription.searchParameters).toBe('a b c');
+    expect(extendedSubscription.lastSeason).toBe(12);
+    expect(extendedSubscription.lastEpisode).toBe(34);
+    expect(typeof extendedSubscription.creationTime).toBe('object');
+    expect(typeof extendedSubscription.lastModified).toBe('object');
+    expect(extendedSubscription.lastSeasonUpdateCheck).toBeUndefined();
+    expect(extendedSubscription.lastEpisodeUpdateCheck).toBeUndefined();
+  });
+
+  it('should return the expected returnable', function () {
+    // the _id and __v field should be removed by getReturnable (but everything else should be unchanged
+    subscriptions.forEach(function (sub) {
+      var returnable,
+        property;
+
+      // (artificially) assign the database fields
+      sub._id = 'foo';
+      sub.__v = 123;
+
+      // get returnable
+      returnable = sub.getReturnable();
+
+      // check whether the database fields have been removed
+      expect(returnable._id).toBeUndefined();
+      expect(returnable.__v).toBeUndefined();
+
+      // iterate over the schema properties
+      for (property in sub.schema.paths) {
+        // skip the database fields...
+        if (property === '_id' || property === '__v') {
+          continue;
+        }
+
+        // evaluate property value (handle Date differently due to the (de-)serialization)
+        if (sub[property] && sub[property].getTime) {
+          expect(new Date(returnable[property]).getTime()).toBe(sub[property].getTime());
+        } else {
+          expect(returnable[property]).toBe(sub[property]);
+        }
+      }
+    });
+  });
+
+  it('should react correctly to (in)valid updateLastEpisode calls', function () {
+    // TODO
+  });
+});
+
