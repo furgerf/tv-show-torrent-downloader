f40041a495dd466744a3e7ca89ee5a84d190452f
Fabian Furger <furgerfabian@hotmail.com>
2016-08-28 18:23:48 +0200

    Add another small test to see if it works...

--- test/torrent-sites/piratebay.js
+++ test/torrent-sites/piratebay.js
@@ -1,10 +1,17 @@
 'use strict';
 
-/*
+var should = require('chai').should(),
+    rewire = require('rewire'),
+    testee = rewire('../../src/torrent-sites/piratebay');
+
 describe('torrent-sites/piratebay', function () {
-  it('SHOULD BE IMPLEMENTED', function () {
-    expect(1).toBe(1);
+  describe('static parse functions', function () {
+    var parseSize = testee.__get__('parseSize'),
+        parseDateThisYear = testee.__get__('parseDateThisYear');
+
+    it('should correctly parse a date this year', function () {
+      parseDateThisYear(["foobar", "1", "2", "3", "4"]).should.eql(new Date(new Date().getFullYear(), 0, 2, 3, 4));
+    });
   });
 });
-*/
 
